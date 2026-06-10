5250824f08505b18b475a1fc86d2aa9b76ac368f
udlox51-creator <udlox51@gmail.com>
2026-06-10 20:43:11 +0500

    Update imega.py

--- imega.py
+++ imega.py
@@ -19,7 +19,7 @@ from telegram.ext import (
 #   SOZLAMALAR
 # ═══════════════════════════════════════════
 BOT_TOKEN      = "8615927460:AAEtFxnz1K5OyqCdcqgVXxu6IxLwT7XWpBg"
-PICWISH_KEY    = os.environ.get("wxlgx7v6nie1bw96f")
+PICWISH_KEY    = os.environ.get("wxhy8t9jo72vcgynr")
 ADMIN_ID       = 8330377593
 KARTA_RAQAM    = "9860080151682814"
 KARTA_EGASI    = "Axadov A"
